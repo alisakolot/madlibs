0903b1b2f725ad08ed26b030bb69b03950d27e2d
Alisa Kolot <kolot.alisa@gmail.com>
2020-07-22 21:14:21 -0500

    rough draft

--- madlibs.py
+++ madlibs.py
@@ -24,7 +24,7 @@ def start_here():
 
 @app.route('/hello')
 def say_hello():
-    """Say hello to user."""
+    """Say hello to user."""  
 
     return render_template("hello.html")
 
@@ -40,6 +40,40 @@ def greet_person():
     return render_template("compliment.html",
                            person=player,
                            compliment=compliment)
+
+
+@app.route('/game')
+def show_madlib_form():
+
+    play_game = request.args.get("play")
+    
+    if play_game == "yes":
+        return render_template("game.html")
+    
+    if play_game == "no":
+        return render_template("goodbye.html")
+
+
+
+@app.route('/madlib')
+def show_madlib():
+    
+    name=request.args.get("celebrity")
+    color=request.args.get("color")
+    noun=request.args.get("noun")
+    adjective=request.args.get("adjective")
+
+    return render_template("game.html",
+                           celebrity=name,
+                           color=color, 
+                           noun=noun,
+                           adjective=adjective)
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
